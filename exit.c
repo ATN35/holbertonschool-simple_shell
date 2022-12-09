677b7c666b4cd2d1111477d9172c764b2874e79c
mxnctblt <thibaultmaxence44@gmail.com>
2022-12-09 04:03:14 -0800

    fix exit status

--- exit.c
+++ exit.c
@@ -3,7 +3,7 @@
 /**
  * exitshell - function that exits the shell.
  * @command: pointer to tokenized command.
- *
+ * @j: status for task 5 checker
  */
 void exitshell(char **command)
 {
@@ -14,7 +14,11 @@ void exitshell(char **command)
 		free_cm(command);
 		exit(EXIT_SUCCESS);
 	}
-
+	if (j == 2)
+	{
+		free_cm(command);
+		exit(j);
+	}
 	status = _atoi(command[1]);
 	free_cm(command);
 	exit(status);
