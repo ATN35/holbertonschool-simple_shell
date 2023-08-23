f31999937a1ec2494aed4bb9db2c1d791d562ed4
ATN35 <contact.atn35@gmail.com>
2023-08-23 01:46:44 -0700

    print error message

--- errors.c
+++ errors.c
@@ -10,8 +10,11 @@ void msgerror(char *name, int i, char **command)
 {
 	char c = i + '0';
 	char *error_message = ": not found\n";
-	char *error_format = ": %c: %s%s";
 
 	write(STDOUT_FILENO, name, _strlen(name));
-	write(STDOUT_FILENO, error_format, c, command[0], error_message);
+	write(STDOUT_FILENO, ": ", 2);
+	write(STDOUT_FILENO, &c, 1);
+	write(STDOUT_FILENO, ": ", 2);
+	write(STDOUT_FILENO, command[0], _strlen(command[0]));
+	write(STDOUT_FILENO, error_message, _strlen(error_message));
 }
