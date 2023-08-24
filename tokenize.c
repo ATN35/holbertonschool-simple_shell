d3a722f19da60f0da2f8a3caf04fc66e942dc9b3
ATN35 <contact.atn35@gmail.com>
2023-08-24 13:19:09 -0700

    Define a function tokenizeCommand() that tokenizes the input command and returns the number of arguments

--- tokenize.c
+++ tokenize.c
@@ -1,10 +1,12 @@
 #include "shell.h"
 
-void tokenizeCommand(char *cmd, char *tokens[]) {
+void tokenizeCommand(char *cmd, char *tokens[])
+{
 	int i = 0;
 	char *token = strtok(cmd, " ");
 
-	while (token != NULL && i < MAX_ARGS - 1) {
+	while (token != NULL && i < MAX_ARGS - 1)
+	{
 		tokens[i] = strdup(token);
 		token = strtok(NULL, " ");
 		i++;
