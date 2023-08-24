88d68d7f8dff0015dcf82b0a4f521af94b4fd5c3
ATN35 <contact.atn35@gmail.com>
2023-08-24 16:09:46 -0700

    Define a function executeCommand() that uses fork() and execve() to execute the provided command

--- execute.c
+++ execute.c
@@ -1,15 +1,21 @@
 #include "shell.h"
 
-void executeCommand(char *cmd, char *args[]) {
+void executeCommand(char *cmd, char *args[])
+{
 	pid_t pid = fork();
 
-	if (pid == 0) {
+	if (pid == 0)
+	{
 		execvp(cmd, args);
 		perror("Erreur lors de l'exécution de la commande");
 		exit(1);
-	} else if (pid < 0) {
+	}
+	else if (pid < 0)
+	{
 		perror("Erreur lors de la création du processus");
-	} else {
+	}
+	else
+	{
 		wait(NULL);
 	}
 }
