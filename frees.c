500bef960d36ec3583690767cf4635dd51dae750
ATN35 <contact.atn35@gmail.com>
2023-08-25 04:05:11 -0700

    The array of tokens to release

--- frees.c
+++ frees.c
@@ -1,8 +1,13 @@
 #include "shell.h"
 
+/**
+ * freeTokens - frees deployed memory for an array of tokens
+ * @tokens: the array of tokens to release
+ */
 void freeTokens(char *tokens[])
 {
 	int i;
+
 	for (i = 0; tokens[i] != NULL; i++)
 	{
 		free(tokens[i]);
