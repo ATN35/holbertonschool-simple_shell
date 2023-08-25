40b687dc737da42fe3aa8d67f0a5cfb3ca6d89a0
ATN35 <contact.atn35@gmail.com>
2023-08-25 03:58:01 -0700

    Implement functions to free allocated memory

--- errors.c
+++ errors.c
@@ -1,17 +1,35 @@
 #include "shell.h"
 
-void commandNotFound(char *cmd) {
+/**
+ * commandNotFound - command not found is replaced by the name of the command
+ * @cmd: pointer to a string
+ */
+void commandNotFound(char *cmd)
+{
 	fprintf(stderr, "Commande introuvable : %s\n", cmd);
 }
+/**
+ * executionError - indicates an error while executing the command
+ * @cmd: pointer to a string
+ */
 
-void executionError(char *cmd) {
+void executionError(char *cmd)
+{
 	perror(cmd);
 }
 
-void forkError(void) {
+/**
+ * forkError - error message indicates an error creating a new process
+ */
+void forkError(void)
+{
 	perror("Erreur lors de la création du processus");
 }
 
-void memoryAllocationError(void) {
+/**
+ * memoryAllocationError - error message indicates memory allocation error
+ */
+void memoryAllocationError(void)
+{
 	perror("Erreur d'allocation de mémoire");
 }
