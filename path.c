c0cbe8b029f969d442917e96ac1eae3fa348b912
ATN35 <contact.atn35@gmail.com>
2023-08-25 01:51:26 -0700

    That retrieves the paths from the environment variable PATH and returns them as an array

--- path.c
+++ path.c
@@ -1,8 +1,14 @@
 #include "shell.h"
 
+/**
+ * getPath - that retrieves the paths from the environment variable PATH
+ * and returns them as an array
+ * Return: Nothing
+ */
 char **getPath(void)
 {
 	char *path = getenv("PATH");
+
 	if (path == NULL)
 	{
 		memoryAllocationError();
