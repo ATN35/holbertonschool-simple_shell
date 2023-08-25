dba171718340aab9aee20d55d49eedc0481957aa
ATN35 <contact.atn35@gmail.com>
2023-08-25 13:21:52 -0700

    Projet simple shell

--- exit.c
+++ exit.c
@@ -8,6 +8,7 @@
 void exitshell(char **command)
 {
 	int status = 0;
+	char status_str[10];
 
 	if (command[1] == NULL)
 	{
@@ -15,6 +16,9 @@ void exitshell(char **command)
 		exit(EXIT_SUCCESS);
 	}
 	status = _atoi(command[1]);
+
+	sprintf(status_str, "%d", status);
+
 	free_cm(command);
 	exit(status);
 }
