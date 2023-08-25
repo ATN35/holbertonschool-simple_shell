47ec883321d6145dccadbd50664ffdc148036343
ATN35 <contact.atn35@gmail.com>
2023-08-25 04:11:00 -0700

    Header file

--- main.c
+++ main.c
@@ -2,27 +2,27 @@
 
 /**
  * main - shell entry point
- * @input: stores user input
- * @args: array to store command arguments
- * @path: array to store PATH paths
- * @i: loop variable
  * Return: always 0
  */
-int main(void) {
+int main(void)
+{
 	char input[MAX_CMD_LENGTH];
 	char *args[MAX_ARGS];
 	char **path = getPath();
 	int i;
 
-	while (1) {
+	while (1)
+	{
 		printPrompt();
-		if (fgets(input, MAX_CMD_LENGTH, stdin) == NULL) {
+		if (fgets(input, MAX_CMD_LENGTH, stdin) == NULL)
+		{
 			printf("\n");
 			break;
 		}
 
 		input[strlen(input) - 1] = '\0';
-		if (strcmp(input, "exit") == 0) {
+		if (strcmp(input, "exit") == 0)
+		{
 			exitShell();
 			break;
 		}
@@ -30,7 +30,8 @@ int main(void) {
 		executeCommand(args[0], args);
 		freeTokens(args);
 	}
-	for (i = 0; path[i] != NULL; i++) {
+	for (i = 0; path[i] != NULL; i++)
+	{
 		free(path[i]);
 	}
 	free(path);
